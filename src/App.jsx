f3c985b03da18d4ffc92c1391ba09cfcc45b4d7b
Gnarlsy <92313945+D-Cahill@users.noreply.github.com>
2022-05-21 14:03:29 +0000

    Added deploy token script

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to IrishDAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
